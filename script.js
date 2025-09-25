d5c4d8962e27eb7ddb30d7ff8baf993315f27c53
Hope Ishaya <hopieslims11@gmail.com>
2025-09-25 12:38:47 +0100

    Update after manual edits

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 // Basic JS for BEJAMAS project
-window.onload = function() {
-  const root = document.getElementById('root');
-  root.innerHTML = '<h1>Welcome to BEJAMAS</h1><p>This is a simple HTML/CSS/JS setup.</p>';
+window.onload = function () {
+  const root = document.getElementById("root");
+  root.innerHTML =
+    "<h1>Welcome to BEJAMAS</h1><p>This is a simple HTML/CSS/JS setup.</p>";
 };
