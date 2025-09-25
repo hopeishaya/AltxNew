f7d65fcd1389d0dccf0711b1c60de886875f8bd7
Hope Ishaya <hopieslims11@gmail.com>
2025-09-25 12:33:20 +0100

    Rearranged project to HTML, CSS, JS structure

---
+++ script.js
@@ -0,0 +1,5 @@
+// Basic JS for BEJAMAS project
+window.onload = function() {
+  const root = document.getElementById('root');
+  root.innerHTML = '<h1>Welcome to BEJAMAS</h1><p>This is a simple HTML/CSS/JS setup.</p>';
+};
